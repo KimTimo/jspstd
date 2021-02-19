cb42e44c1d305ae2019f5eda5cb996aa7ff08c36
KimTimo <eogus8875@naver.com>
2021-02-19 15:43:54 +0900

    This is first Commit!

---
+++ src/com/newlecture/web/Mongshell.java
@@ -0,0 +1,11 @@
+package com.newlecture.web;
+
+public class Mongshell {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is JSP Study!!");
+
+	}
+
+}
---
+++ src/com/newlecture/web/Nana.java
@@ -0,0 +1,29 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hi")
+public class Nana extends HttpServlet
+{
+	public void service(HttpServletRequest request, HttpServletResponse response)
+	                    throws IOException, ServletException
+	{
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		for(int i=0; i<100; i++)
+		{
+			out.println((i+1)+": ¾È³ç Servlet!!<br>");
+		}
+	}
+
+}
